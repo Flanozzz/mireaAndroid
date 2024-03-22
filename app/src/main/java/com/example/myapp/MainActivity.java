d7628578709e7be0dc3a97cc979b37267c49f98a
Flanozzz <belkapult@yandex.ru>
2024-03-22 23:43:12 +0300

    ПР6 коммит 1

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 
 import com.example.myapp.databinding.ActivityMainBinding;
 import com.example.myapp.fragments.pr4.GoToListsFragment;
@@ -14,5 +18,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Intent intent = new Intent(this, OverlayService.class);
+        if (Settings.canDrawOverlays(getApplicationContext())) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(intent);
+            } else {
+                startService(intent);
+            }
+        }
     }
 }
